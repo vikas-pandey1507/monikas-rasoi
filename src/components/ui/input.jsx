9ea5b8f813b8ce123c718c285c87309d76ddb587
vikas-pandey1507 <vikaspandey092015@gmail.com>
2025-11-01 15:59:58 +0400

    Delete src directory

--- src/components/ui/input.jsx
+++
@@ -1,4 +0,0 @@
-
-export function Input(props){
-  return <input {...props} className={`w-full h-10 px-3 rounded-lg border focus:ring-2 focus:ring-black/10 ${props.className||''}`} />
-}
--- src/components/ui/textarea.jsx
+++
@@ -1,4 +0,0 @@
-
-export function Textarea(props){
-  return <textarea {...props} className={`w-full px-3 py-2 rounded-lg border focus:ring-2 focus:ring-black/10 ${props.className||''}`}></textarea>
-}
